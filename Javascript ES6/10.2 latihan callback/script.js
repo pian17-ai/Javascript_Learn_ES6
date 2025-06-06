ed9c819a42fb04509c990e2f693bda37e6614614
Alvian Cahyo Pambudi <alviancahyopambudi@gmail.com>
2025-06-06 16:10:49 +0700

    Update Show Search and Show Detail

--- Javascript ES6/10.2 latihan callback/script.js
+++ Javascript ES6/10.2 latihan callback/script.js
@@ -10,13 +10,43 @@ $.ajax({
                             <div class="card-body">
                                 <h5 class="card-title">${movie.Title}</h5>
                                 <h6 class="card-subtitle mb-2 text-body-secondary">${movie.Year}</h6>
-                                <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#movieDetailModal">Show details</a>
+                                <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${movie.imdbID}" >Show details</a>
                             </div>
                         </div>
                     </div>`
         })
 
         $('.movie-container').html(cards)
+
+        // KETIKA TOMBOL di btn KLIK
+        $('.modal-detail-button').on('click', function() {
+            $.ajax({
+                url: `http://www.omdbapi.com/?apikey=dca61bcc&i=${$(this).data('imdbid')}`,
+                success: result => {
+                    const movieDetail = `<div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <img src="${result.Poster}" class="img-fluid" alt="">
+                                                </div>
+                                                <div class="col-md">
+                                                    <ul class="list-group">
+                                                        <li class="list-group-item"><h4>${result.Title} (${result.Year})</h4></li>
+                                                        <li class="list-group-item"><strong>Director : </strong>${result.Director}</li>
+                                                        <li class="list-group-item"><strong>Actors : </strong>${result.Actors}</li>
+                                                        <li class="list-group-item"><strong>Genre : </strong>${result.Genre}</li>
+                                                        <li class="list-group-item"><strong>Rating : </strong>${result.imdbRating}</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>`;
+
+                    $('.modal-body').html(movieDetail)
+                },
+            error: (e) => {
+                console.log(e.responseText)
+            }
+            });
+        })
         
         // -----------------------------------------------
         // for(let i = 0; i < movies.length; i++) {
